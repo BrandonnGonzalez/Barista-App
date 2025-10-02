25ba4aff38f671509eb8e80521b8353d9690b357
Brandon Gonzalez <brandonyt@icloud.com>
2025-10-02 17:52:18 -0400

    Step 4: Adding functionality to check user inputs against the true result on form submission

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -3,6 +3,7 @@ import RecipeChoices from "./RecipeChoices";
 import drinks from '../drinks.json';
 
 const BaristaForm = () => {
+  // hooks / state variables
   const [currentDrink, setCurrentDrink] = useState('');
   const [trueRecipe, setTrueRecipe] = useState({});
   const [inputs, setInputs] = useState({
@@ -11,107 +12,4 @@ const BaristaForm = () => {
     'syrup': '',
     'blended': ''
   });
-  const ingredients = {
-    'temperature' : ['hot', 'lukewarm', 'cold'],
-    'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
-    'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
-    'blended': ['yes', 'turbo', 'no']
-  }
-
-    const onCheckAnswer = () => {
-
-    }
-
-    const onNewDrink = () => {
-      setInputs({
-        'temperature': '',
-        'milk': '',
-        'syrup': '',
-        'blended': '' });
-        
-      getNextDrink();
-
-    }
-    const getNextDrink = () => {
-      let randomDrinkIndex = Math.floor(Math.random() * drinks.drinks.length);
-      setCurrentDrink(drinks.drinks[randomDrinkIndex].name);
-      setTrueRecipe(drinks.drinks[randomDrinkIndex].ingredients);
-    
-    }
-  
-  return (
-    <div>
-        <h2>Hi, I'd like to order a:</h2>
-        <div className="drink-container"> 
-          <h2 className="mini-header">{currentDrink}</h2>
-          <button className="button newdrink" onClick={onNewDrink}>🔄</button>
-        </div>
-
-        <form>
-        <h3>Temperature</h3>
-<div className="answer-space" >
-  {inputs["temperature"]} 
-</div>
-<RecipeChoices
-  handleChange={(e) => setInputs((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }))}
-  label="temperature"
-  choices={ingredients["temperature"]}
-  checked={inputs["temperature"]}
-/>
-
-<h3>Syrup</h3>
-<div className="answer-space" >
-  {inputs["syrup"]} 
-</div>
-<RecipeChoices
-  handleChange={(e) => setInputs((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }))}
-  label="syrup"
-  choices={ingredients["syrup"]}
-  checked={inputs["syrup"]}
-/>
-
-<h3>Milk</h3>
-<div className="answer-space" >
-  {inputs["milk"]} 
-</div>
-<RecipeChoices
-  handleChange={(e) => setInputs((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }))}
-  label="milk"
-  choices={ingredients["milk"]}
-  checked={inputs["milk"]}
-/>
-
-<h3>Blended</h3>
-<div className="answer-space" >
-  {inputs["blended"]} 
-</div>
-<RecipeChoices
-  handleChange={(e) => setInputs((prevState) => ({
-    ...prevState,
-    [e.target.name]: e.target.value,
-  }))}
-  label="blended"
-  choices={ingredients["blended"]}
-  checked={inputs["blended"]}
-/>
-
-
-        </form>
-        
-        <button className='button-submit' onClick={onCheckAnswer}>Check Answer</button>
-        <button className='button-submit' onClick={onNewDrink}>New Drink</button>
-    </div>
-  );
-  
-};
-
-export default BaristaForm;
+  const [correctTemperature, setCheckedTemperature] = use
--- src/components/RecipeChoices.jsx
+++ src/components/RecipeChoices.jsx
@@ -2,23 +2,4 @@ import { useState } from "react";
 
 const RecipeChoices = ({ handleChange, label, choices, checked }) => {
     return (
-        <div className="radio-buttons">
-        {choices &&
-          choices.map((choice) => (
-            <li key={choice}>
-              <input
-                id={choice}
-                value={choice}
-                name={label}
-                type="radio"
-                onChange={handleChange}
-                checked={checked == choice}
-              />
-                {choice}
-            </li>
-          ))}
-      </div>
-    );
-};
-
-export default RecipeChoices;
+        <div className="radio-but
